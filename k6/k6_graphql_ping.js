a6ea2542b38eb61ec05c708918bf8cacab819a02
xio4 <mapostol@yandex.ru>
2024-11-06 04:36:42 +0500

    feat(bench): add k6 graphql ping

--- k6/k6_graphql_ping.js
+++ k6/k6_graphql_ping.js
@@ -1,19 +1,23 @@
-import { check } from 'k6';
-import http from 'k6/http';
+import { check } from "k6";
+import http from "k6/http";
 
 export const options = {
   thresholds: {
-    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-    http_req_duration: ['p(99)<1000'], // 99% of requests should be below 1s
+    http_req_failed: ["rate<0.01"], // http errors should be less than 1%
+    http_req_duration: ["p(99)<1000"], // 99% of requests should be below 1s
   },
 };
 
 export default function () {
-  const url = 'http://localhost:3000/ping';
+  const url = "http://localhost:3000/ping";
+  const payload = {
+    query: "query Ping {\n  ping {\n    greeting\n    url\n  }\n}",
+    operationName: "Ping",
+  };
 
   const params = {
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   };
 
@@ -22,6 +26,6 @@ export default function () {
 
   // check that response is 200
   check(res, {
-    'response code was 200': (res) => res.status == 200,
+    "response code was 200": (res) => res.status == 200,
   });
 }
